6aa875bb6ea427b3bb62f0b1fdde594dbe7fe360
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-03-02 22:34:47 +0200

    function GenerateBits is done

--- GenerateBits.m
+++ GenerateBits.m
@@ -1,13 +1,18 @@
 function bit_seq = GenerateBits(N_bits)
-%
-% Inputs:
-%   N_bits:     Number of bits in the sequence
-% Outputs:
-%   bit_seq:    The sequence of generated bits
-%
-% This function generates a sequence of bits with length equal to N_bits
+    %
+    % Inputs:
+    %   N_bits:     Number of bits in the sequence
+    % Outputs:
+    %   bit_seq:    The sequence of generated bits
+    %
+    % This function generates a sequence of bits with length equal to N_bits
 
-bit_seq = zeros(1,N_bits);
-%%% WRITE YOUR CODE HERE
-
-%%%
+    bit_seq = zeros(1,N_bits);
+    %%% WRITE YOUR CODE HERE
+    % randi(f, r, c) is a built-in function in matlab that returns a
+    % r x c matrix of random integers in the intervel [1, f] (it starts
+    % from 1 by default) so I made it draw random integers from the
+    % interval [1, 2] then I subtracted 1 to make all the numbers 0s and 1s
+    bit_seq = randi(2,1, N_bits) - 1;
+end
+    %%%
